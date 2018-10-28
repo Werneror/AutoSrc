0e370ea20cb589e800fe5504ad05abdb08660b69
Werner <me@werner.wiki>
2018-10-28 16:39:28 +0800

    改写了craw模块

--- scripts/craw.py
+++ scripts/craw.py
@@ -1,77 +1,87 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# author = w8ay
-import requests
-import re
+# author = Werner
+
 from bs4 import BeautifulSoup
+import requests
 import urlparse
+import urllib
+import Queue
+
 
 class spiderMain(object):
 
-    def __init__(self,url):
+    def __init__(self, url):
         self.SIMILAR_SET = set()
         self.link = url
+        self.domain = self.get_domain(url)
+        self.proto = urllib.splittype(url)[0]
 
-    def judge(self,url):
-        # 先将URL链接，然后判断是否在origin
-        # 在判断?/aspx/asp/php/jsp 是否在里面
-        origin = self.link
-        new_url = urlparse.urljoin(origin,url)
-        domain = urlparse.urlparse(origin).netloc
-        
-        if domain not in new_url:
+    def get_domain(self, url):
+        return urllib.splithost(urllib.splittype(url)[1])[0]
+
+    def judge(self, url):
+        if url.startswith('javascript'):
             return False
-        if self.url_similar_check(new_url) == False:
-            return False
-        if '=' in new_url and ('aspx' in new_url or 'asp' in new_url or 'php' in new_url or 'jsp' in new_url):
-            return new_url
+        if url.startswith('http://') or url.startswith('https://') or url.startswith('//'):
+            domain = self.get_domain(url)
+            if domain != self.domain:
+                return False
+            if url.startswith('//'):
+                url = self.proto + ':' + url
+        elif url.startswith('/'):
+            url = urlparse.urljoin(self.proto + '://' + self.domain, url)
         else:
-            return False
-
-    def url_similar_check(self,url):
-        '''
-        URL相似度分析
-        当url路径和参数键值类似时，则判为重复
-        '''
-        url_struct = urlparse.urlparse(url)
-        query_key = '|'.join(sorted([i.split('=')[0] for i in url_struct.query.split('&')]))
-        url_hash = hash(url_struct.path + query_key)
-        if url_hash not in self.SIMILAR_SET:
-            self.SIMILAR_SET.add(url_hash)
-            return True
-        return False
+            url = urlparse.urljoin(self.url, url)
+        return url
 
     def run(self):
         header = dict()
         header["User-Agent"] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"
-        header["Referer"] = "http://www.qq.com"
+        header["Referer"] = "http://www.baidu.com/"
         new_urls = set()
         try:
             r = requests.get(self.link, headers=header, timeout=5)
             if r.status_code == 200:
                 soup = BeautifulSoup(r.text, 'html.parser')
-                
-                links = soup.find_all('a')
-                for link in links:
-                    new_url = link.get('href')
+                a_tags = soup.find_all('a')
+                for a_tag in a_tags:
+                    new_url = a_tag.get('href')
                     full_url = self.judge(new_url)
                     if full_url:
                         new_urls.add(full_url)
-            else:
-                return False
         except Exception:
-            return False
-        finally:
-            if new_urls:
-                return new_urls
-            else:
-                return False
+            pass
+        return new_urls
+
 
 def poc(url):
     if '://' not in url:
         url = 'http://' + url
-    if not url.endswith('/'):
-        url = url + "/"
-    s = spiderMain(url)
-    f = s.run()
-    return f
+
+    urls = set()
+    urls.add(url)
+
+    url_queue = Queue.Queue()
+    url_queue.put((0, url))
+    while not url_queue.empty():
+        level, url = url_queue.get()
+        if level > 3:
+            break
+        s = spiderMain(url)
+        new_urls = s.run()
+        for url in new_urls:
+            urls.add(url)
+            url_queue.put((level+1, url))
+
+    return urls
+
+
+if __name__ == '__main__':
+    domain = 'http://asset.pingan.com/gongkaixinxi/jiben.shtml'
+    urls = poc(domain)
+    if urls:
+        for url in urls:
+            print(url)
+    else:
+        print('None')
